53185e37033ea0ab88d19b941b298ec3f55d9579
Tin Barbarić <tin.bar10@gmail.com>
2026-02-24 16:37:28 +0100

    Gradimo bazu znanja i dodajemo embeddings-e (vektore) i upsert-amo ih

--- ingest.py
+++ ingest.py
@@ -77,10 +77,12 @@ def main():
     print(f"[1/5] Ucitavam embedding model: {EMBED_MODEL_NAME}")
     embedder = SentenceTransformer(EMBED_MODEL_NAME)
 
+
     # 2) chromadb lokalno inicija.
     print(f"[2/5] Inicijaliziram ChromaDB u: {CHROMA_DIR}")
     client = chromadb.PersistentClient(path=CHROMA_DIR, settings=Settings(anonymized_telemetry=False))
     collection = client.get_or_create_collection(name=COLLECTION_NAME, metadata={"hnsw:space": "cosine"})
+
 
     # 3) citamo pdf-ove
     pdf_files = get_pdf_files(DATA_DIR)
@@ -90,6 +92,53 @@ def main():
 
     print(f"[3/5] Pronadjeno PDF-ova: {len(pdf_files)}")
     total_chunks = 0
+    
+    
+    # 4) build-amo bazu znanja (file-ove)
+    ids: List[str] = []
+    documents: List[str] = []
+    metadatas: List[Dict[str, Any]] = []
+
+    for pdf_path in pdf_files:
+        source_name = os.path.basename(pdf_path)
+        print(f"  - Citanje: {source_name}")
+
+        pages = read_pdf_pages(pdf_path)
+        for page_no, page_text in pages:
+            chunks = chunk_text(page_text, CHUNK_SIZE, CHUNK_OVERLAP)
+            for ci, chunk in enumerate(chunks):
+                doc_id = str(uuid.uuid4())
+                ids.append(doc_id)
+                documents.append(chunk)
+                metadatas.append({
+                    "source": source_name,
+                    "page": page_no,
+                    "chunk_index": ci,
+                })
+                total_chunks += 1
+
+    if total_chunks == 0:
+        print("Nisam izvukao tekst (0 chunkova). Provjeri PDF (možda je sken bez teksta).")
+        return
+
+    print(f"[4/5] Ukupno chunkova: {total_chunks}. Radim embedding...")
+
+
+    # 5) embeding i upsert (update + insert)
+    embeddings = embedder.encode(documents, show_progress_bar=True, normalize_embeddings=True).tolist()
+
+    print("[5/5] Spremanje u Chroma kolekciju...")
+    collection.upsert(
+        ids=ids,
+        documents=documents,
+        metadatas=metadatas,
+        embeddings=embeddings,
+    )
+
+    print("\n Gotovo!")
+    print(f"Kolekcija: {COLLECTION_NAME}")
+    print(f"Chroma path: {CHROMA_DIR}")
+    print(f"Broj dokumenata u kolekciji: {collection.count()}")
 
 
 if __name__ == "__main__":
